346577dc4c6e9afe94e95f1a073adea69d87afc4
NithinkumarSR <cecsnithinkumarsr24@gamil.com>
2023-05-04 14:20:10 +0530

    fact

--- js.js
+++ js.js
@@ -11,15 +11,21 @@ const firebaseConfig = {
   };
   firebase.initializeApp(firebaseConfig);
   const detail=[];
+  const fact_detail=[];
   const user_database=firebase.database().ref(" user databasestore");
-  const faculty_database=firebase.database().ref("faculty databasestore");
-
+  const faculty_database=firebase.database().ref("databasestore");
   user_database.on("value",function(snapshot){
   snapshot.forEach(function(element){
     var a=element.val();
   detail.push(a);
   });
   });
+  faculty_database.on("value",function(snapshot){
+    snapshot.forEach(function(element){
+      var b=element.val();
+    fact_detail.push(b);
+    });
+    });
 const login=document.querySelector('.stud');
 const login1=document.querySelector('.login');
 document.querySelector('#click').addEventListener('click',function()
@@ -52,6 +58,20 @@ else
     document.querySelector('#Uname').value = "";
     login1.classList.add('hidden');
     alert('wrong password');
+}
+}
+else if(Role()=="faculty"){
+for(let i=0;i<fact_detail.length;i++){
+  console.log(fact_detail[i]);
+  if(fact_detail[i]==document.querySelector('#Uname').value && document.querySelector('#Pass').value=="cse"){
+    c1=1;
+  }
+}
+if(c1==1){
+  window.location.replace('faculty.html');
+}
+else{
+  alert('wrong user');
 }
 }
 else{
